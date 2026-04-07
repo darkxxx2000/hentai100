7a0525fe7e37e30e1f45833f30042ef06a5e94b6
darkxxx2000 <silvana1002010@gmail.com>
2026-04-07 14:11:01 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 {
-  title: "Deep Sea Curse",
-  image: "https://...",
+  title: "Taimanin Asagi (2009)",
+  image: "https://static.wikia.nocookie.net/taimanin/images/f/f3/Tadvdcover.jpg/revision/latest?cb=20180924031208",
   tags: ["tentacles","monster"],
   color: "tentacles"
 },
