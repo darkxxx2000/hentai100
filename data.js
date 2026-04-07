6845f413203501cc101c51afb5b46da73da4bcab
darkxxx2000 <silvana1002010@gmail.com>
2026-04-07 14:10:10 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,18 @@
-const posts = [
-  { title:"Taimanin Asagi (2009)", image:"https://static.wikia.nocookie.net/taimanin/images/f/f3/Tadvdcover.jpg/revision/latest?cb=20180924031208", tags:["tentacles","monster"] },
-  { title:"OVA 2", image:"https://...", tags:["machine","3d"] },
-  { title:"OVA 3", image:"https://...", tags:["monster"] },
-  { title:"OVA 4", image:"https://...", tags:["tentacles"] },
-  { title:"OVA 5", image:"https://...", tags:["3d"] },
-  { title:"OVA 6", image:"https://...", tags:["machine"] },
-  { title:"OVA 7", image:"https://...", tags:["monster"] },
-  { title:"OVA 8", image:"https://...", tags:["tentacles"] },
-  { title:"OVA 9", image:"https://...", tags:["3d"] },
-  { title:"OVA 10", image:"https://...", tags:["machine"] }
-];
+{
+  title: "Deep Sea Curse",
+  image: "https://...",
+  tags: ["tentacles","monster"],
+  color: "tentacles"
+},
+{
+  title: "Romantic Ecchi Story",
+  image: "https://...",
+  tags: ["romantico","ecchi"],
+  color: "romantico"
+},
+{
+  title: "Mechanical Obsession",
+  image: "https://...",
+  tags: ["machine"],
+  color: "machine"
+}
