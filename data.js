84b74ab4475d0694941768a65f4a3b05915555d8
darkxxx2000 <silvana1002010@gmail.com>
2026-04-07 13:58:21 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,12 @@
 const posts = [
-  {
-    title: "OVA Title 1",
-    description: "Brief editorial description of the OVA.",
-    image: "https://imagehost.com/path/cover1.jpg",
-    tags: ["tentacles", "monster"]
-  },
-  {
-    title: "OVA Title 2",
-    description: "Another description here.",
-    image: "https://imagehost.com/path/cover2.jpg",
-    tags: ["machine", "3d"]
-  }
+  { title:"OVA 1", image:"https://...", tags:["tentacles","monster"] },
+  { title:"OVA 2", image:"https://...", tags:["machine","3d"] },
+  { title:"OVA 3", image:"https://...", tags:["monster"] },
+  { title:"OVA 4", image:"https://...", tags:["tentacles"] },
+  { title:"OVA 5", image:"https://...", tags:["3d"] },
+  { title:"OVA 6", image:"https://...", tags:["machine"] },
+  { title:"OVA 7", image:"https://...", tags:["monster"] },
+  { title:"OVA 8", image:"https://...", tags:["tentacles"] },
+  { title:"OVA 9", image:"https://...", tags:["3d"] },
+  { title:"OVA 10", image:"https://...", tags:["machine"] }
 ];
