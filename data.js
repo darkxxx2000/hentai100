70d24e4e9e2b9f1038c932f8e64c8eef999e51a7
darkxxx2000 <silvana1002010@gmail.com>
2026-04-07 13:36:58 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const posts = [
+  {
+    title: "OVA Title 1",
+    description: "Brief editorial description of the OVA.",
+    image: "assets/covers/ova1.jpg",
+    tags: ["tentacles", "monster"]
+  },
+  {
+    title: "OVA Title 2",
+    description: "Another description here.",
+    image: "assets/covers/ova2.jpg",
+    tags: ["machine", "3d"]
+  }
+];
