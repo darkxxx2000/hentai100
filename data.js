475b26b921b3e8115b34541e8221dbd371ffb88b
darkxxx2000 <silvana1002010@gmail.com>
2026-04-09 16:06:50 -0300

    Update data.js

--- data.js
+++ data.js
@@ -12,15 +12,15 @@ const posts = [
 
     // 👇 9 thumbnails horizontales (3x3 perfectos)
     thumbnails: [
-      "https://via.placeholder.com/640x360?text=Extra+1",
-      "https://via.placeholder.com/640x360?text=Extra+2",
-      "https://via.placeholder.com/640x360?text=Extra+3",
-      "https://via.placeholder.com/640x360?text=Extra+4",
-      "https://via.placeholder.com/640x360?text=Extra+5",
-      "https://via.placeholder.com/640x360?text=Extra+6",
-      "https://via.placeholder.com/640x360?text=Extra+7",
-      "https://via.placeholder.com/640x360?text=Extra+8",
-      "https://via.placeholder.com/640x360?text=Extra+9"
+      "https://i.ytimg.com/vi/AZT8RVrXRrA/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLC8tqcK6IsL_vVqsIp2Qzvf9OpOxw",
+      "https://thumb-cdn77.xvideos-cdn.com/b6b2f72e-9572-4821-bc13-d6defb9ab5e7/0/xv_8_t.jpg",
+      "https://i0.wp.com/v2.hentaihd.net/wp-content/uploads/2022/01/Taimanin-Asagi-Episode-1_9.jpg",
+      "https://watchhentai.net/uploads/2022/12/taimanin-asagi/2.jpg",
+      "https://watchhentai.net/uploads/2022/12/taimanin-asagi/backdrop1.jpg",
+      "https://hstream.moe/images/hentai/taimanin-asagi/gallery-ep-3-0.webp",
+      "https://preview.redd.it/taimanin-asagi-episode-2-sakura-beloved-v0-jtoktfbjn9rb1.jpg?width=640&crop=smart&auto=webp&s=9d6be6659c14cf61b969deb7d3b81e1084637d03",
+      "https://hstream.moe/images/hentai/taimanin-asagi/gallery-ep-4-0-thumbnail.webp",
+      "https://wimg.rule34.xxx//images/1904/126817dfafdbb165e5816190f22b70c1b2aab6b1.gif?2061040"
     ]
   },
 
